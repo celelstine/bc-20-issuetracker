3cf6f412804bb515cddb3ef14a4e22415f614d3a
celestine <okorocelestine@gmail.com>
2017-04-20 02:43:36 +0100

    add complete to notification address

--- public/js/issue.js
+++ public/js/issue.js
@@ -1,57 +1,51 @@
 //var utility = require('lib/utility.js');
 $(document).ready(function(){ 
-  //password strength
-  $("#password").keypress(function() {
-   // alert($("#password").val());
+  $("#notifymeans").change(function () {
+     let valueSelected  = $("#notifymeans").val(),
+         Userref = firebase.database().ref('ist/user'),
+         usercontact,
+         uid=localStorage.uid;
+      console.log(valueSelected);
+      Userref.orderByChild('uid').equalTo(uid).on("value", function(snapshot) {
+        //console.log(snapshot.val());
+        snapshot.forEach(function(data) {
+          if (valueSelected === "phone") {
+            usercontact = data.val().phone;
+          } else if (valueSelected === "email")
+            usercontact = data.val().email;
+        });
+      $("#notifyvalue").val(usercontact);
+      });
   });
 
-  $("#registration").submit(function(event) {
+  $("#issue").submit(function(event) {
 
     /* stop form from submitting normally */
     event.preventDefault();
-      /** using back end
-      // get the action attribute from the <form action=""> element 
-      var $form = $( this ),
-        url = $form.attr( 'action' );
-      var posting = $.post( url, { name: $('#name').val(), email: $('#email').val(), department: $('#department').val()} );
-
-      //posting.done(function( data ) {
-       // $("#result").text(data);
-
-      //});$('#password').val()
-      **/
-  //create user 
-  firebase.auth().createUserWithEmailAndPassword($('#email').val(), $('#password').val())
-  .then(function(user){
-    var currentUser = firebase.auth().currentUser;
-    if(currentUser) {
-       localStorage.uid = currentUser.uid;
-    }
-
-  })
-  .catch(function(error) {
-  // Handle Errors here.
-  var errorCode = error.code;
-  var errorMessage = error.message;
-  // ...
-  });
-  userRef = firebase.database().ref('ist/user');
-
-  var  newuser = {
-    "name" : $('#name').val(),
-    "departments" : $('#department').val(),
-    "uid" : localStorage.uid,
-    "email" : $('#email').val(),
-    "phone" :$('#phone').val()
-  };
-  //console.log(localStorage.uid);
-  userRef.push(newuser).then(function(user) {
-    window.location.href = '/'
+    let Issueref = firebase.database().ref('ist/issue'),
+       newissue = {
+        "raisedby" : localStorage.uid,
+        "dateraised" : new Date().toLocaleString(),
+        "status" : "Initiated",
+        "description" :$('#description').val() ,
+        "department" : $('#department').val(),
+        "subject" : $('#subject').val(),
+        "comment" : {
+        },
+        "lastupdate" : new Date().toLocaleString(),
+        "timeclose" : "",
+        "sendernotificationmeans"  : $("#notifymeans").val(),
+        "notificationvalue" : $("#notifyvalue").val(),
+        "isnotified" : false,
+        "fixernote" : ""
+      };
+  console.log(newissue);
+  Issueref.push(newissue).then(function(issue) {
+    window.location.href ='/issuelog';
   }). catch(function(error) {
+    console.error('Sign Out Error', error);
     $("#result").text = "Error occures, please try again";
-    //console.error('Sign Out Error', error);
-    
   });
-    });
+  });
 });
   
