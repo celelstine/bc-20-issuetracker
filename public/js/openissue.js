34d35664907d6182b147ccd80221ce885e091236
celestine <okorocelestine@gmail.com>
2017-04-20 16:44:51 +0100

    show assignee

--- public/js/openissue.js
+++ public/js/openissue.js
@@ -4,16 +4,17 @@ $(document).ready(function(){
  			Issueref = firebase.database().ref('ist/issue'),
  			curissue ;
  			console.log(department);
+ 	
  Issueref.orderByChild('department').equalTo(department).on("value", function(snapshot) {
   //console.log(snapshot.val());
 	  snapshot.forEach(function(data) {
 	   	curissue = data.val();
 	  	if (curissue.status != 'Closed') {
 	  		displayIssue(data.key,curissue.priority,curissue.raisedby,curissue.subject,curissue.dateraised,curissue.description,
-	        					curissue.department,curissue.status,curissue.lastupdate,curissue.assigneeID,curissue.assigneeName);
+	        					curissue.department,curissue.status,curissue.lastupdate,curissue.assignto,curissue.assigneeName);
 	       let test = curissue.raisedby + '-' +curissue.subject+ '-' +curissue.dateraise+ '-' +curissue.description
-	        							+ '-' +curissue.department+ '-' +curissue.status+ '-' +curissue.lastupdate;
-	        	//console.log(data.val());
+	        							+ '-' +curissue.department+ '-' +curissue.status+ '-' +curissue.assigneeto;
+	       console.log(data.val());
 	  	}  	
 	  });
 	});
@@ -35,12 +36,13 @@ $(document).ready(function(){
  	//process assign form
  	$("#setAssign").submit(function(event) {
     /* stop form from submitting normally */
-    alert('test');
     event.preventDefault();
+    	//collect the issue id
+		let issueid = localStorage.getItem('issueid');
     // set assignto in db
-    let Issueref = firebase.database().ref('ist/issue').child(fixedid),
+    let Issueref = firebase.database().ref('ist/issue').child(issueid),
     		assignee = $('#assignee').val();
-    		assigneeName = $('#assignee').text();
+    		assigneeName = $('#assignee option:selected').html();
 	  Issueref.once('value', function(snapshot) {
 			if( snapshot.val() != null ) {
 				console.log(snapshot.val());
@@ -48,12 +50,9 @@ $(document).ready(function(){
 		  }
 		});
 		// change button to label
-		//collect the issue id
-		let issue = localStorage.getItem('issueid');
-		// change button to label
-	  $('#btn'+ issue).hide();
-	  $('#lbl'+ issue).text(assigneeName)
-	  $('#lbl'+ issue).show();
+	  $('#btn'+ issueid).hide();
+	  $('#lbl'+ issueid).text(assigneeName)
+	  $('#lbl'+ issueid).show();
 
   });
 
@@ -82,7 +81,7 @@ var displayIssue = function(key,priority,raisedBy,subject,dateraise,description,
 	        '<td>' + todate(dateraise) + '</td>' +
 	        '<td> <button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#assign" ' +
 	        'onclick=\'assign("'+key+ '")\' id="btn'+ key+'" >Assign</button> ' +
-	        '<label id="lbl'+ key+'" text ="'+assigneeName+'"></label>  </td>' +
+	        '<label id="lbl'+ key+'" >'+assigneeName+'</label>  </td>' +
 	        '<td> <button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#description" onclick=\'viewdescription("'+description+ '")\'>View Description</button> </td>' +
 	        '<td> <button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#closeissue" onclick=\'closeissue("'+key+ '")\'>close issue</button> </td>' +
 	        '</tr>';
